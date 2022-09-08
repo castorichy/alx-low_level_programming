fd28b242629415eb90e8fd822a6287b589c5259c
castorichy <castorichy@gmail.com>
2022-09-08 12:23:45 -0700

    puts a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 
 /*
- * a function thats prints a sentance using puts
+ *main - utputing a string using puts function
+ *
+ *return: succesfull
  */
-int main(void)
+int main`(void)
+/*
+ * "Programming is like building a multilingual
+ */
 {
 	char name[100] = "\"Programming is like building a multilingual puzzle";
 
 	puts(name);
+	/*
+	 *returns 0 if no error
+	 */
 	return (0);
 }
