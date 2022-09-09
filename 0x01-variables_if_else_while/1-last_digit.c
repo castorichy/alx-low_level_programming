ca6b98a7c81091ba35769a49f1856e8cd8fffc8c
castorichy <castorichy@gmail.com>
2022-09-09 06:51:06 -0700

    is greater than five?

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - main Entry
+ *
+ *  Return: Always return 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int a = n % 10;
+
+	/*checking if the last digit of a number*/
+	if (a > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	}
+	else if (a < 6 && a != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is zero\n", n, a);
+	}
+	return (0);
+}
