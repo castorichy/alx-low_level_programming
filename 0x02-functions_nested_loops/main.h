022a703c9b2cf0243d0c81880b170ff53020cd4b
castorichy <castorichy@gmail.com>
2022-09-14 06:02:51 -0700

    edited

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-/**
- * putchar - Prints out a char
- *
- * Return: Always Return 0
- */
-int _putchar(void)
-{
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
-}
+#ifndef main.h
+#define main.h
+
+int _putchar();
+
+#endif
