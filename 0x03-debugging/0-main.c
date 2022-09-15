a2205673fea6704dae06fb6995b90f84a8c68fe1
castorichy <castorichy@gmail.com>
2022-09-15 05:22:05 -0700

    positive commited

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - checks if the number is positive or negative
+ * @i: stores an int
+ * Return: NULL
+ */
+void positive_or_negative(int i)
+{
+	if (i == 0)
+	{
+		printf("%d is zero", i);
+	}
+
+}
