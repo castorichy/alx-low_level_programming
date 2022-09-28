0d34f1121c8b72ee27209f21dc670b3882869cb0
castorichy <castorichy@gmail.com>
2022-09-28 19:30:23 +0300

    initial commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,18 +5,17 @@
  * @n: number to get its factorials
  *
  * Return: return n
- * 	returns -1 if n < 0 to indicate an error
+ * returns -1 if n < 0 to indicate an error
  */
 
 int factorial(int n)
 {
 	int s;
-	if (n < 0 )
+
+	if (n < 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	else
-		s = n * factorial(n - 1);
-
+	s = n * factorial(n - 1);
 	return (s);
 }
