6d32b731e4e021942756849378820a0aa8016773
castorichy <castorichy@gmail.com>
2022-10-03 18:58:20 +0300

    creat array of string

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *chr;
 
-	chr = malloc(sizeof(char)* size);
+	chr =(char*)malloc(sizeof(char)* size);
 
 	if (size == 0)
 		return (NULL);
