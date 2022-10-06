ff7c0301318201de687b530de39a51a94a96e2e2
castorichy <castorichy@gmail.com>
2022-10-06 17:15:58 +0300

    Alocate mem for array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,14 +11,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *arr;
+	char *arr;
+	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
 		return (NULL);
 
 	arr = malloc(nmemb * size);
 
 	if (arr == NULL)
 		exit(0);
+	for (i = 0; i< nmemb * size; i++)
+		arr[i] = 0;
 	return (arr);
 }
