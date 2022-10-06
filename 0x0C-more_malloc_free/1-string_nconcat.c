9e78dc3b902d8f09604099a5697f60db4485f36a
castorichy <castorichy@gmail.com>
2022-10-06 14:36:51 +0300

    concatenate two str

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,11 +19,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len_s1 = len_s2 = total_len = 0;
 
 	if (s1 == NULL)
-		return (" ");
+		i = 0;
 	if (s2 == NULL)
-		return (" ");
-	if (n <= 0)
-		return (" ");
+		j = 0;
 
 	/* length of s1 and s2 */
 	for (i = 0; *(s1 + i); i++)
@@ -46,7 +44,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len_s1; i++)
 		ptr[i] = s1[i];
 	for (j = 0; j < n; j++)
-		ptr[i + j + '\0'] = s2[j];
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
 
 
 	return (ptr);
