9c38174a4a674f651c63b5a21225811c7cb70a32
castorichy <castorichy@gmail.com>
2022-10-11 15:27:29 +0300

    puppy details

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - contains dog details
+ * @name: dog name
+ * @age: dogs age
+ * @owner: owner of the dog
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner; 
+};
+
+#endif
