90388778c31ff4fe1fec58377ed9bab1259a6d57
castorichy <castorichy@gmail.com>
2022-10-11 16:29:19 +0300

    puppy function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include "dog.h"
+#include <string.h>
 
 /**
  * init_dog - intializes dog structure
@@ -9,7 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
