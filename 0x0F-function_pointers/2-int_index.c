f5696e37ebc29366f2b5b1b65a1d78a4fe89432d
castorichy <castorichy@gmail.com>
2022-10-13 21:42:43 +0300

    added: array search pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, ch;
 
-	if ((array) && (size <= 0) && cmp)
+	if ((!array) && (size <= 0) && !cmp)
 		return (-1);
 
 	for (i = 0; i < size; i++)
