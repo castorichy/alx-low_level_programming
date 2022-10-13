b205e6cc3a36dfb3dbed8505345015d8a27e1b29
castorichy <castorichy@gmail.com>
2022-10-13 21:31:56 +0300

    added: array search pointer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - gets the index matched
+ * @array: contains array of integers
+ * @size: size of the array
+ * @cmp: points to function to campaire
+ *
+ * Return: return index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, ch;
+
+	if ((!array) && (size <= 0))
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		ch = cmp(array[i]);
+		if (ch)
+			return (i);
+	}
+	return (-1);
+}
