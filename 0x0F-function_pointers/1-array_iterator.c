c316a42cdc80687f2fedb18324efdd375588db48
castorichy <castorichy@gmail.com>
2022-10-13 16:32:49 +0300

    added: array funct pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates the valus of an array
+ * @array: array of integers
+ * @size: size of array
+ * @action: function pointer that points to a function we will be using
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
