b5c83ca4ff16c1bcb03c16bd67b1e63747ce71a0
castorichy <castorichy@gmail.com>
2022-10-14 13:46:20 +0300

    add all number in arg

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef VARIADIC_FUNCTIONS_H_
+#define VARIADIC_FUNCTIONS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif /*VARIADIC_FUNCTIONS_H_*/
