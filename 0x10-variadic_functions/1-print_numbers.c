7cd2446afc9343990034937fd16a5a1c960cd50d
castorichy <castorichy@gmail.com>
2022-10-14 22:13:19 +0300

    so errors updated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			printf("%d", va_arg(op, int));
 			if (i < n - 1)
-				printf(",");
+				printf("%s",separator);
 		}
 		printf("\n");
 		va_end(op);
