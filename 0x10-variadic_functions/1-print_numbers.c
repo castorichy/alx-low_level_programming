792347123307aa27693c0ecf407cd3eacbbd380d
castorichy <castorichy@gmail.com>
2022-10-14 22:05:47 +0300

    print all args int

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print all args passed
+ * @separator: char used to separate to ints
+ * @n: nomber of args passed
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	if (separator != NULL || n != 0)
+	{
+		unsigned int i;
+
+		va_list op;
+
+		va_start(op, n);
+
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(op, int));
+			if (i < n - 1)
+				printf(",");
+		}
+		printf("\n");
+		va_end(op);
+	}
+}
