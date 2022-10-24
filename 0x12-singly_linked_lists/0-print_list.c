ae2115e0e40e02bba7bd14c453b6a17aa48477ff
castorichy <castorichy@gmail.com>
2022-10-24 23:20:23 +0300

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,14 +10,14 @@
 size_t print_list(const list_t *h)
 {
 	const list_t *nil;
-	size_t i;
+	unsigned int i;
 
 	nil = h;
 
 	for (i = 0; nil; i++)
 	{
 		if (nil->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", nil->len, nil->str);
 		nil = nil->next;
