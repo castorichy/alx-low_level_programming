94b1d9a37d2f06e1d6969e3085e9fd3808686181
castorichy <castorichy@gmail.com>
2022-10-24 17:33:40 +0300

    list all elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints list
+ * @n: pointer to struct
+ *
+ * Return: length of str
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		while(h->str)
+			n++;
+		printf("%s", h->str);
+	}
+	return (n);
+}
