87bf988b9c9db5fb98c7d7f43baec4adf22c8a31
castorichy <castorichy@gmail.com>
2022-10-25 12:26:00 +0300

    inserting a new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,27 +11,23 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode;
-	list_t *old = *head;
-	unsigned int i = 0;
+	size_t len = 0, i = 0;
 
 	newnode = malloc(sizeof(list_t));
 	if (!newnode)
 	{
 		return (NULL);
 	}
-	else
+	newnode->next = *head;
+	newnode->str = strdup(str);
+	while (str[i] != '\0')
 	{
-		newnode->next = old;
-		newnode->str = strdup(str);
-		while (str[i] != '\0')
-		{
-			i++;
-		}
-		newnode->len = i++;
-
-
-		old = newnode;
+		len++;
+		i++;
 	}
-	return (old);
+	newnode->len = i++;
+
+	*head = newnode;
+	return (*head);
 	free(newnode);
 }
