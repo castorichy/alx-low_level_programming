bf1509241ab64022b599ac887109ce252fef6060
castorichy <castorichy@gmail.com>
2022-10-26 13:22:01 +0300

    list all int in linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all int in the list
+ * @h: pointer to listint_t
+ * Return: size_t numer of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
