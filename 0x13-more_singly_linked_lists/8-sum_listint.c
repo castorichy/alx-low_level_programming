8932ba260948899bde73c0658d99c60cca4d39b2
castorichy <castorichy@gmail.com>
2022-10-28 23:33:03 +0300

    sum all elements in list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sums all elements in a list
+ * @head: head of thefirst node
+ *
+ * Return: sum
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	while (head->next)
+	{
+		head = head->next;
+		sum = sum + head->n;
+	}
+	return (sum);
+}
