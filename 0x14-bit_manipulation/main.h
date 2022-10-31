7d5e5552e022d93e4f54a08b8392a377ab241027
castorichy <castorichy@gmail.com>
2022-10-31 23:05:09 +0300

    binary to decimal

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* _MAIN_H*/
