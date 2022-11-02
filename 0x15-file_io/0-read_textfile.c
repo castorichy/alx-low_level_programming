6185a74e11ddf3abd93cd0d2f3e43f7c4e0c6451
castorichy <castorichy@gmail.com>
2022-11-03 00:07:20 +0300

    reading a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,41 +10,41 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t wfd, rfd;
-    char *str;
+	int fd;
+	ssize_t wfd, rfd;
+	char *str;
 
-    if (!filename)
-        return (0);
-    
-    /*creating a file*/
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-    
-    /* reading and sending file contents to std out*/
-    str = malloc(sizeof(char) * letters);
-    if (!str)
-    {
-        free(str);
-        return (0);
-    }
+	if (!filename)
+		return (0);
+	
+	/*creating a file*/
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	
+	/* reading and sending file contents to std out*/
+	str = malloc(sizeof(char) * letters);
+	if (!str)
+	{
+		free(str);
+		return (0);
+	}
 
-    rfd = read(fd, str, letters);
-    close(fd);
-    if (rfd == -1)
-        return (0);
+	rfd = read(fd, str, letters);
+	close(fd);
+	if (rfd == -1)
+		return (0);
 
-    /*Writing into the file*/
-    wfd = write(STDOUT_FILENO, str, letters);
-    free(str);
-    if (wfd == -1)
-        return (0);
-    /*dprintf(rfd, "%s", str);*/
-    /*closing the file*/
+	/*Writing into the file*/
+	wfd = write(STDOUT_FILENO, str, letters);
+	free(str);
+	if (wfd == -1)
+		return (0);
+	/*dprintf(rfd, "%s", str);*/
+	/*closing the file*/
 
-    if (rfd != wfd)
-       return (0);
-    return (wfd);
+	if (rfd != wfd)
+	   return (0);
+	return (wfd);
 
 }
