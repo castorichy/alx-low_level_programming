1886c9604fdd34fa6c121e7dda59c905ce50e194
castorichy <castorichy@gmail.com>
2022-12-09 10:54:11 +0300

    Add all values in a node

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * 
+ *
 */
 
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
 
-    if (head == NULL)
-        return (0);
-    while (head)
-    {
-        sum += head->n;
-        head = head->next;
-    }
-    return (sum);
+	if (head == NULL)
+		return (0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
