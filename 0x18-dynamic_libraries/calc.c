fc8e1e7c2895c18ccfe55faed0ccbc98ddcac293
castorichy <castorichy@gmail.com>
2022-12-13 15:12:06 +0300

    import to python from c

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,66 @@
+#include "calc.h"
+
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum of the two numbers
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the diffrence of the two numbers
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the multiplication of the two numbers
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - mod two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the reminder of the two numbers
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * div - div two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the division of the two numbers
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/calc.h
@@ -0,0 +1,13 @@
+#ifndef _CALC_H_
+#define _CALC_H_
+
+#include <stdio.h>
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+
+#endif /*_CALC_H_*/
